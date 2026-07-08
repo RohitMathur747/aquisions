fbba3c2cb1224293e180fc691a4317096f8c5d03
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-07-08 18:07:07 +0530

    Implement Eslint and Prettier is Done!!!

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("hello from Acqusions!");
+app.get('/', (req, res) => {
+  res.status(200).send('hello from Acqusions!');
 });
 
 export default app;
