9f4d118edc028f9dff0b107a52493389a08c6607
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-07-15 16:55:29 +0530

    Day 11 - Secure Api with Arject Done!!!

---
+++ src/Middleware/security.Middleware.js
@@ -0,0 +1,84 @@
+import aj from '#config/arject.js';
+import { slidingWindow } from '@arcjet/node';
+import logger from '#config/logger.js';
+//import { slidingWindow } from '@arcjet/node';
+
+const securityMiddleware = async (req, res, next) => {
+  try {
+    const role = req.user?.role || 'guest';
+    let limit;
+    let message;
+
+    switch (role) {
+      case 'admin':
+        limit = 20;
+        message = 'Admin request is exceeds by (20 per minute),Slow down';
+        break;
+
+      case 'user':
+        limit = 10;
+        message = 'User request is exceeds by (10 per minute),Slow down';
+        break;
+
+      case 'guest':
+        limit = 5;
+        message = 'Guest request is exceeds by (5 per minute),Slow down';
+        break;
+    }
+
+    const client = aj.withRule(
+      slidingWindow({
+        node: 'LIVE',
+        interval: '1m',
+        max: limit,
+        name: '${role}-rate-limit',
+      })
+    );
+    const decision = await client.protect(req);
+    if (decision.isDenied() && decision.reason.isBot()) {
+      (logger.warn('Bot request Blocked'),
+        { ip: req.ip, userAgent: req.get('User Agent'), path: req.path });
+      return res.status(403).json({
+        error: 'Forbidden',
+        message: 'Automated requests are not allowed',
+      });
+    }
+
+    if (decision.isDenied() && decision.reason.isShield()) {
+      (logger.warn('Shield Blocked request'),
+        {
+          ip: req.ip,
+          userAgent: req.get('User Agent'),
+          path: req.path,
+          method: req.method,
+        });
+      return res.status(403).json({
+        error: 'Forbidden',
+        message: 'Request block by security policy',
+      });
+    }
+
+    if (decision.isDenied() && decision.reason.isRateLimit()) {
+      (logger.warn('rate Limit exceeded'),
+        {
+          ip: req.ip,
+          userAgent: req.get('User Agent'),
+          path: req.path,
+          method: req.method,
+        });
+      return res.status(403).json({
+        error: 'Forbidden',
+        message: 'Too many Requests',
+      });
+    }
+    next();
+  } catch (error) {
+    console.error('Arject middleware error', error);
+    res.status(500).json({
+      error: 'Internal Server Error',
+      message: 'Something went wrong with security middleware',
+    });
+  }
+};
+
+export default securityMiddleware;
