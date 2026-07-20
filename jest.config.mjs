723f5a6f84bc22a75bef2f4a0e5727711f1feac2
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-07-20 18:39:36 +0530

    test 1 - ci/cd pipeline testing

--- jest.config.mjs
+++ jest.config.mjs
@@ -91,7 +91,17 @@ const config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^#src/(.*)$': '<rootDir>/src/$1',
+    '^#config/(.*)$': '<rootDir>/src/config/$1',
+    '^#controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^#middleware/(.*)$': '<rootDir>/src/middleware/$1',
+    '^#models/(.*)$': '<rootDir>/src/models/$1',
+    '^#routes/(.*)$': '<rootDir>/src/routes/$1',
+    '^#services/(.*)$': '<rootDir>/src/services/$1',
+    '^#utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^#validations/(.*)$': '<rootDir>/src/validations/$1',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
